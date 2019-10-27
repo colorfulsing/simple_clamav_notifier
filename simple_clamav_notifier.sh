216f5ca3e5660533810692d7d9057db9f8e7245a
colorfulsing <47130084+colorfulsing@users.noreply.github.com>
2019-10-27 12:13:26 -0600

    Add quarentine support.

--- simple_clamav_notifier.sh
+++ simple_clamav_notifier.sh
@@ -3,6 +3,18 @@ found=
 last_line=
 last_line_timeout=0
 timeout_runtime="2 seconds"
+# Quarentine support, enabled = 1, disabled = 0
+# Default = 0
+quarentine_enabled=0
+quarentine_dir="/quarentine"
+log_file="/var/log/clamav/simple_clamav_notifier.log"
+
+# Create quarentine directory if don't exists
+if [ "$quarentine_enabled" == "1" ] && [ ! -d "$quarentine_dir" ]; then
+  mkdir -p "$quarentine_dir"
+fi
+
+echo "Simple clamav notifier has started" >> "$log_file"
 
 tail -fn0 /var/log/clamav/clamd.log | \
 while read line ; do
@@ -24,13 +36,32 @@ while read line ; do
       # Alert detection
       virus_file="$(echo "$line" | grep -oP "ScanOnAccess: \K(.*)(?=:(.*) FOUND)")"
       virus_name="$(echo "$line" | grep -oP "ScanOnAccess: .*?: \K(.*)(?= FOUND)")"
-      env CLAM_VIRUSEVENT_VIRUSNAME="$virus_name" CLAM_VIRUSEVENT_FILENAME="$virus_file" /etc/clamav/detected.sh
+      file_in_quarentine="$(if [ "$quarentine_enabled" == "1" ] && [[ "$virus_file" == "${quarentine_dir}/"* ]]; then echo "1"; else echo "0"; fi)"
+      if [ "$quarentine_enabled" != "1" ] || [ "$file_in_quarentine" != "1" ]; then
+        env CLAM_VIRUSEVENT_VIRUSNAME="$virus_name" CLAM_VIRUSEVENT_FILENAME="$virus_file" /etc/clamav/detected.sh
+      fi
       
-      # Uncommend to send to quarantine
-      #if [ -n "virus_file" ]; then
-      #  mv virus_file /path/to/quarantine/
-      #  echo "Found and quarantined virus file: virus_file" >> /var/log/clamav/clamd.log
-      #fi
+      # Uncomment to send to quarantine
+      if [ "$quarentine_enabled" == "1" ] && [ -f "$virus_file" ] && [ "$file_in_quarentine" != "1" ]; then
+        # Create quarentine directory if don't exists
+        if [ ! -d "$quarentine_dir" ]; then
+          mkdir -p "$quarentine_dir"
+        fi
+
+        # Quarentine file, remove execution rights and change user to clamav
+        echo "Trying to quarentine virus file \"$virus_file\"..." >> "$log_file"
+        quarentine_file="$quarentine_dir/$(date +%s%N)_${virus_file##*/}" && \
+        chmod ugo-x "$virus_file" && \
+        mv "$virus_file" "$quarentine_file" && \
+        chown clamav:clamav "$quarentine_file" && \
+        chmod o-rw "$quarentine_file" && \
+        echo "Found and quarentined virus file \"$virus_file\" as \"$quarentine_file\"" >> "$log_file"
+
+        # Check if successful
+        if [ -f "$virus_file" ]; then
+          echo "Failed to quarentine \"$virus_file\" virus file!!!" >> "$log_file"
+        fi
+      fi
     fi
   fi
 done
